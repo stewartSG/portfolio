e4fe94de9c09490c800ac52138e5881b731a5c57
StewartSG <stewart.addmein@gmail.com>
2016-06-01 01:31:26 +0800

    Added hibari page, disabled turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+// require turbolinks
 //= require vendor/jquery-2.1.4.min
-//= require_tree . 
+//= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root 'pages#homepage'
+
   post 'submit_email' => 'pages#submit_email'
+  get 'hibari' => 'pages#hibari'
+  get 'home' => 'pages#homepage'
+
+
 end
